6713e65c09bb6446f908217ddadf0c7783edcba9
RoniTuohimaa <t1turo00@students.oamk.fi>
2023-03-19 00:06:49 +0200

    Rekisteröinti, kirjautuminen sekä vaikeusastevalikko lisätty

---
+++ app/src/main/java/com/example/speedmathv2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.speedmathv2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        val EasyButton: Button = findViewById(R.id.buttonEasy)
+        EasyButton.setOnClickListener{
+            val intent = Intent(this, EasyActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
