120b20aef688f2d5ba6b6858314537e38a2b258f
OnniRaisanen <t1raon00@students.oamk.fi>
2023-04-04 14:43:18 +0300

    Google sign in

--- app/src/main/java/com/example/speedmathv2/MainActivity.kt
+++ app/src/main/java/com/example/speedmathv2/MainActivity.kt
@@ -7,12 +7,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //sign out button
+        auth= FirebaseAuth.getInstance()
+        findViewById<Button>(R.id.buttonSignOut).setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
 
 
 
